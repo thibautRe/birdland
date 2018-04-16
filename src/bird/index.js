2e9f886cb64ae6f78c6c603072e1d62d30e67139
Thibaut REMY <thibaut@domora.com>
2018-04-16 22:12:30 +0200

    :wrench: Add stage-0 babel preset

--- src/bird/index.js
+++ src/bird/index.js
@@ -1,41 +1,20 @@
 import Phaser from 'phaser'
 
 export default class Bird extends Phaser.GameObjects.Sprite {
-  static get key() {
-    return 'bird'
-  }
-  static get sprite() {
-    return require('./bird.png')
-  }
-  static get spriteDimensions() {
-    return { frameWidth: 32, frameHeight: 22 }
-  }
-
+  static key = 'bird'
   static preload(scene) {
-    scene.load.spritesheet(Bird.key, Bird.sprite, Bird.spriteDimensions)
-    scene.load.image(
-      'particle',
-      'http://labs.phaser.io/assets/particles/blue.png',
-    )
+    scene.load.spritesheet(Bird.key, require('./bird.png'), {
+      frameWidth: 32,
+      frameHeight: 22,
+    })
   }
 
   constructor({ scene, x, y }) {
     super(scene, x, y, Bird.key, 0)
 
-    const particles = this.scene.add.particles('particle')
-    const emitter = particles.createEmitter({
-      speed: 200,
-      scale: { start: 1, end: 0 },
-      blendMode: 'ADD',
-    })
-
     this.type = 'bird'
-
     this.scene.physics.world.enable(this)
     this.scene.add.existing(this)
-
-    emitter.startFollow(this)
-
     this.scene.anims.create({
       key: 'down',
       frames: this.scene.anims.generateFrameNumbers(Bird.key, {
