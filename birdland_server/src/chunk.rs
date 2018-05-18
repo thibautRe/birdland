1f4898575a03612dfe48bf45faa92c00686e2934
Thibaut REMY <thibaut@thibaut.re>
2018-05-18 08:22:29 +0200

    :sparkles: Add Island Chunk function

--- birdland_server/src/chunk.rs
+++ birdland_server/src/chunk.rs
@@ -5,7 +5,7 @@ use self::grid_2d::{Coord, Grid, Size};
 use self::noise::{NoiseFn, Perlin, Seedable};
 
 /// Size of a chunk
-const _CHUNK_SIZE: usize = 128;
+const _CHUNK_SIZE: usize = 64;
 
 /// A Chunk holds informations about a size and position,
 /// as well as informations for randomization
@@ -13,6 +13,7 @@ pub struct Chunk {
     pub size: u32,
     pub position: Coord,
     noises: Vec<Noise>,
+    seed: u32,
 }
 
 impl Chunk {
@@ -28,24 +29,64 @@ impl Chunk {
             position,
             size: _CHUNK_SIZE as u32,
             noises,
+            seed,
         }
     }
 
     /// Returns a Grid with noised values
     pub fn get_grid(&self) -> Grid<f64> {
         let mut grid = Grid::new_default(Size::new(_CHUNK_SIZE as u32, _CHUNK_SIZE as u32));
+        let is_island_chunk = self.is_island_chunk();
         for coord in grid.coords() {
             let mut value = 0.0;
+            // Calculate the resulting noise from the perlins
+            // The real coord needs to be shifted by the origin
+            // of the grid
+            let moved_coord = coord + (self.position * _CHUNK_SIZE as i32);
             for noise in self.noises.iter() {
-                value += noise.get(coord) * noise.amplitude;
+                value += noise.get(moved_coord) * noise.amplitude;
             }
 
+            // If the chunk is an island then higher it!
+            if is_island_chunk {
+                value *= 1.0 + (self.get_island_factor(coord) * 10.0);
+            }
+
+            // Assign the noise value to the coord
             if let Some(x) = grid.get_mut(coord) {
                 *x = value;
             }
         }
 
         grid
+    }
+
+    /// Returns true if the chunk contains an island
+    fn is_island_chunk(&self) -> bool {
+        let noise = Noise::new(self.seed, 30.0, 1.0);
+
+        println!("Island : {}", noise.get(self.position).abs() < 0.5);
+        noise.get(self.position).abs() < 0.5
+    }
+
+    /// Returns a value between 0 and 1 (0 at the edges of the chunk,
+    /// in order to get a smooth transition between chunks)
+    fn get_island_factor(&self, point: Coord) -> f64 {
+        // lower it closer to 1 for more rough edges
+        // increase it to make it narrower
+        let factor: f64 = 100.0;
+
+        // Formulae : e^(-1/x + 1/(x-CHUNK))
+        // TODO: Probably replace that by something else, gives too squarish results
+        let factor_x =
+            factor.powf(-1.0 / point.x as f64 + 1.0 / (point.x as f64 - _CHUNK_SIZE as f64));
+        let factor_y =
+            factor.powf(-1.0 / point.y as f64 + 1.0 / (point.y as f64 - _CHUNK_SIZE as f64));
+
+        // The edge factor is 0 at the edges and around 1 in the center
+        let edge_factor = factor_x * factor_y * 1.4;
+
+        edge_factor
     }
 }
 
@@ -64,8 +105,6 @@ impl Noise {
         let scale = target_scale * (1.0 + generator.get([0.9, 0.9]));
         let amplitude = target_amplitude * (1.0 + generator.get([0.1, 0.1]));
 
-        println!("scale {}, amplitude {}", scale, amplitude);
-
         Self {
             amplitude,
             scale,
@@ -74,8 +113,10 @@ impl Noise {
     }
 
     fn get(&self, point: Coord) -> f64 {
-        self.generator
-            .get([point.x as f64 * self.scale, point.y as f64 * self.scale])
+        let noise = self.generator
+            .get([point.x as f64 * self.scale, point.y as f64 * self.scale]);
+        // Normalize the value between 0 and 1 (the noise generator yields -1 and 1)
+        (noise + 1.0) / 2.0
     }
 }
 
--- birdland_server/src/main.rs
+++ birdland_server/src/main.rs
@@ -3,31 +3,32 @@ extern crate noise;
 
 pub mod chunk;
 
-use grid_2d::Coord;
 use chunk::Chunk;
+use grid_2d::Coord;
 
 fn main() {
-    let chunk = Chunk::new(6, Coord::new(0, 0));
+    // 3, 0 is an island
+    let chunk = Chunk::new(6, Coord::new(4, 0));
     let grid = chunk.get_grid();
 
     for coord in grid.coords() {
         let mut value = grid.get(coord).unwrap();
-        let value = ((value + 1.0) * 5.0).round() as i32;
+        let value = value.round() as i32;
         if coord.x == 0 {
             print!("\n");
         }
 
         let character = match value {
-            1 => ' ',
-            2 => '.',
-            3 => '-',
-            4 => '=',
-            5 => '+',
-            6 => '*',
-            7 => '#',
-            8 => '%',
-            9 => '@',
-            10 => '▓',
+            0 => ' ',
+            1 => '-',
+            2 => '=',
+            3 => '+',
+            4 => 'x',
+            5 => 'å',
+            6 => '%',
+            7 => '$',
+            8 => 'M',
+            9 => '▓',
             _ => ' ',
         };
 
